aafc7dd7ecbb41960790958042c909a4facabf97
CodeBite <giovaniportal@gmail.com>
2024-10-10 22:32:54 -0700

    adding status custom

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ for (const folder of commandFolders) {
 // When the client is ready
 client.once(Events.ClientReady, async readyClient => {
     console.log(`Ready! Logged in as ${readyClient.user.tag}`);
-    client.user.setActivity('Sharks Win 🦈', { type: ActivityType.Watching });
+    client.user.setActivity('Sharks Lost!🦈', { type: ActivityType.Custom });
 
     // Deploy commands
     await deployCommands(clientId, guildId, token); // Pass the required parameters
