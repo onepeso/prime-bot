c01ee3514525805024e46194d46c039d709072a0
CodeBite <giovaniportal@gmail.com>
2024-10-10 21:38:21 -0700

    added test command to try things

---
+++ commands/utility/testCommand.js
@@ -0,0 +1,11 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('test')
+        .setDescription('Test Command Nothing Useful'),
+
+    async execute(interaction) {
+        await interaction.reply('Test Command! Seeing if something works.');
+    },
+};
