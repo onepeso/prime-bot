fecebf6c6796e83d4da1149a02757980036649db
CodeBite <giovaniportal@gmail.com>
2024-10-10 22:08:02 -0700

    Added insult test

--- commands/utility/testCommand.js
+++ commands/utility/testCommand.js
@@ -1,4 +1,5 @@
 const { SlashCommandBuilder} = require('discord.js');
+const { request } = require('undici');
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -7,5 +8,15 @@ module.exports = {
 
     async execute(interaction) {
         await interaction.reply('Test Command! Seeing if something works.');
+        const apiUrl = `https://evilinsult.com/generate_insult.php?lang=en&type=json`;
+        try {
+            const { body } = await request(apiUrl);
+            const data = await body.json();
+            await interaction.followUp(data.insult);
+            console.log(data.insult);
+        } catch (error) {
+            console.error('Error fetching insult:', error);
+            await interaction.followUp('There was an error retrieving the insult. Please try again later.');
+        }
     },
 };
