c1a4281e280ea86f44ff7aded8b531fecfb148e0
CodeBite <giovaniportal@gmail.com>
2024-10-10 22:14:55 -0700

    testing insult

--- commands/utility/testCommand.js
+++ commands/utility/testCommand.js
@@ -7,12 +7,14 @@ module.exports = {
         .setDescription('Test Command Nothing Useful'),
 
     async execute(interaction) {
+
+        const userMentioned = interaction.options.getUser('user');
         await interaction.reply('Test Command! Seeing if something works.');
         const apiUrl = `https://evilinsult.com/generate_insult.php?lang=en&type=json`;
         try {
             const { body } = await request(apiUrl);
             const data = await body.json();
-            await interaction.followUp(data.insult);
+            await interaction.followUp(`${data.insult} ${userMentioned}`);
         } catch (error) {
             console.error('Error fetching insult:', error);
             await interaction.followUp('There was an error retrieving the insult. Please try again later.');
